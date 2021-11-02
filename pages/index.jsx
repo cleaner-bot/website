2c55d1a7afe828db02a99ed0ee79707fbf8c0f5f
Leo Developer <leodeveloper@protonmail.com>
2021-11-02 20:05:46 +0100

    initial commit

---
+++ pages/index.jsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+    return (
+        <>
+            <p>
+                No idea how you got here, but you are wrong here.
+            </p>
+        </>
+    )
+}
