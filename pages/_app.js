13bb8c151cbedc7931aaa3994b81977d6434f845
Leo Developer <leodeveloper@protonmail.com>
2021-11-17 16:42:48 +0100

    add html language tag

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,15 @@
 
 import React from "react";
+import Head from "next/head";
 
 import "../styles/globals.css";
 
 export default function MyCustomApp({ Component, pageProps }) {
     return (
         <>
+            <Head>
+                <html lang="en" />
+            </Head>
             <Component {...pageProps} />
         </>
     )
