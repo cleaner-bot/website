6354ef3d1668a4ca109e996fcb9ce383f70712e3
Leo Developer <leodeveloper@protonmail.com>
2021-11-17 16:42:42 +0100

    add 404

---
+++ pages/404.jsx
@@ -0,0 +1,22 @@
+import { ArrowLeftIcon } from "@heroicons/react/outline";
+
+export default function FourOFour() {
+    return (
+        <div className="flex justify-center items-center my-10">
+            <div>
+                <p className="text-9xl font-extrabold border-b-4 border-indigo-500 text-center">
+                    404
+                </p>
+                <p className="text-4xl font-bold text-gray-100 text-center mt-2">
+                    Page not found
+                </p>
+                <button className="-anim -btn w-full bg-blue-580 hover:bg-blue-630 mt-8" onClick={() => {
+                    window.history.back();
+                }}>
+                    <ArrowLeftIcon className="w-6 h-6 mr-2" />
+                    Go back
+                </button>
+            </div>
+        </div>
+    )
+}
