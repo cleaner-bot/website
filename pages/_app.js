441b09d882b60a8e5578cff7eb2e8225940abb3c
Leo Developer <leodeveloper@protonmail.com>
2021-11-17 17:25:09 +0100

    fix colossal error

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,11 @@
 
 import React from "react";
-import Head from "next/head";
 
 import "../styles/globals.css";
 
 export default function MyCustomApp({ Component, pageProps }) {
     return (
         <>
-            <Head>
-                <html lang="en" />
-            </Head>
             <Component {...pageProps} />
         </>
     )
