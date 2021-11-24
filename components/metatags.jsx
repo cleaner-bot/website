36c3d3ab1df44728824dd8d5da32e5b4344fa3fd
Leo Developer <leodeveloper@protonmail.com>
2021-11-24 20:37:30 +0100

    does this work? :eyes:

--- components/metatags.jsx
+++ components/metatags.jsx
@@ -7,11 +7,11 @@ export default function MetaTags({ title, description }) {
         <Head>
             <title>{title}</title>
             <meta name="description" content={description} />
-            <meta name="theme-color" content="#6366F1" />
             <meta property="og:title" content={title} />
             <meta property="og:type" content="article" />
             <meta property="og:description" content={description} />
             <meta property="og:image" content="https://cdn.discordapp.com/avatars/823533449717481492/64e20328c02c26f8da22e43b17f2d0db.png?size=256" />
+            <meta property="og:color" content="#6366F1" />
             <meta name="twitter:card" content="summary" />
         </Head>
     )
