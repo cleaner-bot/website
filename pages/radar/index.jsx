c08f4ddbf1f740251e2318f6784b9cf02f5aca16
Leo Developer <leodeveloper@protonmail.com>
2021-12-05 02:00:55 +0100

    update data and give a bit of text

--- pages/radar/index.jsx
+++ pages/radar/index.jsx
@@ -1,9 +1,8 @@
 
 import MetaTags from "@/components/metatags.jsx";
 
-
 export default function Radar() {
-    const data = {'rules': {'phishing': {'instant': 89, 'not_instant': 11}, 'categories': {'Advertisements': 6, 'Phishing': 35, 'Other': 2, 'Antispam': 56}}, 'challenges': {'action': {'captchas': 11, 'bans': 89}, 'categories': {'Phishing': 40, 'Antispam': 58, 'Advertisements': 1, 'Other': 0}}, 'last_data': '2021-12-03T02:17:09'}
+    const data = {'rules': {'phishing': {'instant': 89, 'not_instant': 11}, 'categories': {'Advertisement': 8, 'Phishing': 80, 'Other': 4, 'Antispam': 5, 'Selfbot': 3}}, 'challenges': {'action': {'captchas': 28, 'bans': 72}, 'categories': {'Phishing': 90, 'Antispam': 5, 'Advertisement': 1, 'Other': 1, 'Selfbot': 4}}, 'last_data': '2021-12-04T21:26:09'}
     const sorted_rules_categories = Object.keys(data.rules.categories).map(x => ({name: x, value: data.rules.categories[x]})).sort((a, b) => b.value - a.value);
     const sorted_challenge_categories = Object.keys(data.challenges.categories).map(x => ({name: x, value: data.challenges.categories[x]})).sort((a, b) => b.value - a.value);
     return (
@@ -18,6 +17,11 @@ export default function Radar() {
             </h1>
             <p>
                 Last update: {new Date(data.last_data).toLocaleString()}
+            </p>
+            <p className="mt-8">
+                The Cleaner is a Discord bot for keeping your servers clean.
+                The Cleaner Radar is powered by real data we collect anonymously to provide you (and us) with statistics.
+                This will stay relatively simple for now, because we do not have much data to show off.
             </p>
             <h2 className="text-2xl font-bold mt-20 mb-4">
                 Rules
@@ -130,3 +134,26 @@ export default function Radar() {
         </div>
     )
 }
+
+/*
+
+                    <p className="h-6">
+                        {barState >= 10 && barState < 10 + sorted_challenge_categories.length && <>
+                            Selected: {sorted_challenge_categories[barState - 10].name}  ({sorted_challenge_categories[barState - 10].value}%)
+                        </>}
+                    </p>
+                    <BarChart layout="vertical" width={600} height={50} data={[data.challenges.categories]} className="w-full">
+                        <XAxis hide type="number" />
+                        <YAxis hide dataKey="name" type="category" />
+                        <Tooltip />
+                        {sorted_challenge_categories.map((x, i) => <Bar
+                            dataKey={x.name}
+                            stackId="a"
+                            fill={categoryColors[x.name]}
+                            onMouseOver={() => setBarState(10 + i)}
+                            className={barState === 10 + i && "scale-y-110 origin-center"}
+                            radius={i === 0 ? [10, 0, 0, 10] : i === sorted_challenge_categories.length - 1 ? [0, 10, 10, 0] : undefined}
+                            key={x.name}
+                        />)}
+                    </BarChart>
+*/
