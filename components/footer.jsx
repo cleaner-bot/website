173d84d8f2c5523dfff505897e465b466d686a31
Leo Developer <leodeveloper@protonmail.com>
2022-03-05 16:23:21 +0100

    add link to discord and fix legal links

--- components/footer.jsx
+++ components/footer.jsx
@@ -11,6 +11,7 @@ const footer = [
         links: [
             { name: "Home", href: "/" },
             { name: "Dashboard", href: "/dash/" },
+            { name: "Discord", href: "/discord" }
         ]
     },
     {
@@ -40,13 +41,13 @@ export default function Footer({ className }) {
             <div className="col-span-3 lg:col-span-1">
                 <Image className="w-24 h-24 col-span-2 mx-auto rounded-full bg-gray-710" src="/img/avatar.png" alt="The Cleaner Logo" />
                 <div className="flex justify-center gap-4 mt-2">
-                    <Link href="/terms">
+                    <Link href="/legal/terms">
                         <a className="hover:underline">Terms</a>
                     </Link>
-                    <Link href="/privacy">
+                    <Link href="/legal/privacy">
                         <a className="hover:underline">Privacy</a>
                     </Link>
-                    <Link href="/impressum">
+                    <Link href="/legal/impressum">
                         <a className="hover:underline">Impressum</a>
                     </Link>
                 </div>
@@ -71,85 +72,6 @@ export default function Footer({ className }) {
                     </Link>)}
                 </div>
             </div>)}
-        </div>
-    )
-}
-
-
-function FooterX({ className }) {
-    return (
-        <div className={clsx("grid grid-cols-1 sm:grid-cols-2 gap-4 pb-10 xl:grid-cols-4 gap-y-12", className)}>
-            <div>
-                <Link href="/">
-                    <a className="text-3xl font-bold hover:underline">
-                        The Cleaner
-                    </a>
-                </Link>
-                <p className="mt-1 text-gray-200">
-                    Copyright &copy; 2021 - 2022
-                </p>
-                <p className="text-gray-200">
-                    <Link href="https://leodev.xyz">
-                        <a className="hover:underline text-whites">
-                            Leo Developer
-                        </a>
-                    </Link>.
-                </p>
-                <p className="mt-2 text-gray-200">
-                    Not affiliated with Discord.
-                </p>
-            </div>
-            <div>
-                <h3 className="text-sm text-gray-200">
-                    Services
-                </h3>
-                <div className="flex flex-col gap-2 mt-4 md:gap-0 md:mt-0">
-                    <Link href="/dash">
-                        <a className="hover:underline">Dashboard</a>
-                    </Link>
-                    {/* <Link href="/docs/workers/">
-                        <a className="hover:underline">Workers</a>
-                    </Link> */}
-                </div>
-            </div>
-            <div>
-                <h3 className="text-sm text-gray-200">
-                    Information
-                </h3>
-                <div className="flex flex-col gap-2 mt-4 md:gap-0 md:mt-0">
-                    <Link href="/docs">
-                        <a className="hover:underline">Documentation</a>
-                    </Link>
-                    <Link href="/blog">
-                        <a className="hover:underline">Blog</a>
-                    </Link>
-                    <Link href="/status">
-                        <a className="hover:underline">System Status</a>
-                    </Link>
-                    <Link href="/radar">
-                        <a className="hover:underline">Radar</a>
-                    </Link>
-                </div>
-            </div>
-            <div>
-                <h3 className="text-sm text-gray-200">
-                    Legal
-                </h3>
-                <div className="flex flex-col gap-2 mt-4 md:gap-0 md:mt-0">
-                    <Link href="/privacy">
-                        <a className="hover:underline">Privacy Policy</a>
-                    </Link>
-                    <Link href="/impressum">
-                        <a className="hover:underline">Impressum</a>
-                    </Link>
-                    <Link href="/terms">
-                        <a className="hover:underline">Terms of Use</a>
-                    </Link>
-                    <Link href="/discord">
-                        <a className="hover:underline">Contact (Discord)</a>
-                    </Link>
-                </div>
-            </div>
         </div>
     )
 }
