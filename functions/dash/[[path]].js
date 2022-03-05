111519b795f548d3a054fc8ac40b03391c50d8fb
Leo Developer <leodeveloper@protonmail.com>
2022-03-05 20:26:56 +0100

    pls work

--- functions/dash/[[path]].js
+++ functions/dash/[[path]].js
@@ -1,6 +1,7 @@
 export function onRequestGet({ env, request, params }) {
     let { path } = params;
     path = path.slice(1).join("/");
-    return new Response(`${new URL("/dash/[id]/" + path, request.url).toString()} ${path}`)
-    // return env.ASSETS.fetch(new Request(new URL("/dash/[id]/" + path, request.url).toString(), request))
+    const url = new URL("/dash/[id]" + (path ? "/" + path : ""), request.url);
+    // return new Response(`${url.toString()} ${path}`)
+    return env.ASSETS.fetch(new Request(url.toString(), request))
 }
