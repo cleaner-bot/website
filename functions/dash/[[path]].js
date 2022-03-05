4a3d9f0d79be9d30024f45d701ae4c5943131ec1
Leo Developer <leodeveloper@protonmail.com>
2022-03-05 20:21:36 +0100

    fix js again

--- functions/dash/[[path]].js
+++ functions/dash/[[path]].js
@@ -1,7 +1,7 @@
 export function onRequestGet({ env, request, params }) {
     try {
         let { path } = params;
-        path = path.split("/").slice(1).join("/");
+        path = path.slice(1).join("/");
         return env.ASSETS.fetch(new Request(new URL("/dash/[id]/" + path, request.url).toString(), request))
     } catch(e) {
         return new Response(`${e.message} ${e.stack} ${typeof(params.path)} ${params.path}`)
