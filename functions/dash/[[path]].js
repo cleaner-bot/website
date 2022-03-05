0579bacd3c0538d81f39dcf12ab73a1d59aceeb7
Leo Developer <leodeveloper@protonmail.com>
2022-03-05 20:08:43 +0100

    fix more js

--- functions/dash/[[path]].js
+++ functions/dash/[[path]].js
@@ -1,4 +1,4 @@
-export async function onRequest({ env, request, params }) {
+export function onRequest({ env, request, params }) {
     let { path } = params;
     path = path.split("/").slice(1).join("/");
     return env.ASSETS.fetch(new Request(new URL("/dash/[id]/" + path, request.url).toString(), request))
