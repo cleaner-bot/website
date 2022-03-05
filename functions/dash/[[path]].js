277ff68a41ad4b3adfb92c88b0238a6cead2a1e3
Leo Developer <leodeveloper@protonmail.com>
2022-03-05 20:13:33 +0100

    add a fucking error handler

--- functions/dash/[[path]].js
+++ functions/dash/[[path]].js
@@ -1,5 +1,9 @@
 export function onRequestGet({ env, request, params }) {
-    let { path } = params;
-    path = path.split("/").slice(1).join("/");
-    return env.ASSETS.fetch(new Request(new URL("/dash/[id]/" + path, request.url).toString(), request))
+    try {
+        let { path } = params;
+        path = path.split("/").slice(1).join("/");
+        return env.ASSETS.fetch(new Request(new URL("/dash/[id]/" + path, request.url).toString(), request))
+    } catch(e) {
+        return new Response(`${e.message} ${e.stack}`)
+    }
 }
