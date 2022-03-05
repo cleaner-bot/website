0182c1cefd005b223e32a0dab6e66416fa89b6c1
Leo Developer <leodeveloper@protonmail.com>
2022-03-05 20:31:16 +0100

    nvm need the error handler again

--- functions/dash/[[path]].js
+++ functions/dash/[[path]].js
@@ -1,5 +1,9 @@
 export function onRequestGet({ env, request, params }) {
-    let { path } = params;
-    const url = new URL(path.length === 0 ? "/dash" : "/dash/[id]" + (path.length > 1 ? "/" + path.slice(1).join("/") : ""), request.url);
-    return env.ASSETS.fetch(new Request(url.toString(), request))
+    try {
+        let { path } = params;
+        const url = new URL(path.length === 0 ? "/dash" : "/dash/[id]" + (path.length > 1 ? "/" + path.slice(1).join("/") : ""), request.url);
+        return env.ASSETS.fetch(new Request(url.toString(), request))
+    } catch(e) {
+        return new Response(`${e.message} ${e.stack}`);
+    }
 }
