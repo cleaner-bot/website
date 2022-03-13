0a141af56081fe95d101afa606a8116fed287bb9
Leo Developer <git@leodev.xyz>
2022-03-13 10:12:54 +0100

    improve error and state handling of challenge

--- pages/challenge/index.jsx
+++ pages/challenge/index.jsx
@@ -6,54 +6,50 @@ import clsx from "clsx";
 
 import { getChallenge, createOAuthRedirect, postChallenge } from "@/lib/api.js";
 import { InternalLink } from "@/components/buttons.jsx";
+import ErrorHandler from "@/components/dash/error";
 
 
 export default function ChallengeWrapper() {
     const router = useRouter();
-    const [response, setResponse] = useState();
-    const [step, setStep] = useState(0);
+    const [state, setState] = useState({ stage: 0 });
 
     useEffect(async () => {
         if(!router.isReady) return;
         const { flow } = router.query;
-        if(!flow) {
-            alert("Missing flow parameter.");
-            return;
-        }
+        if(!flow)
+            return router.push("/");
 
         let response;
         try {
             response = await getChallenge(flow);
         } catch(e) {
-            return alert(`${e.response.status}: ${e.response.data.detail}`);
+            return setState({ ...state, error: e });
         }
-        setResponse(response);
         if(response.data.correct_account && !response.data.captcha) {
-            setStep(3);
+            setState({ stage: 3, response });
             try {
                 await postChallenge(flow);
             } catch (e) {
-                setStep(1);
-                return alert(`${e.response.status}: ${e.response.data.detail}`);
+                return setState({ ...state, error: e });
             }
-            setStep(4);
+            setState({ stage: 4, response });
         } else {
-            setStep(response.data.correct_account ? 2 : 1);
+            setState({ stage: response.data.correct_account ? 2 : 1, response });
         }
     }, [router])
 
     const steps = [
-        { id: "Step 1", name: "Loading challenge", status: step > 0 ? "complete" : "current" },
-        { id: "Step 2", name: "Discord authentication", status: step > 1 ? "complete" : step === 1 ? "current" : "upcoming" },
-        { id: "Step 3", name: "CAPTCHA challenge", status: step > 2 ? (response.data.captcha ? "complete" : "skipped") : step === 2 ? "current" : "upcoming" },
-        { id: "Step 4", name: "Verifying information", status: step > 3 ? "complete" : step === 3 ? "current" : "upcoming" },
-        { id: "Step 5", name: "Done", status: step === 4 ? "current" : "upcoming" },
+        { id: "Step 1", name: "Loading challenge", status: state.stage > 0 ? "complete" : "current" },
+        { id: "Step 2", name: "Discord authentication", status: state.stage > 1 ? "complete" : state.stage === 1 ? "current" : "upcoming" },
+        { id: "Step 3", name: "CAPTCHA challenge", status: state.stage > 2 ? (state.response.data.captcha ? "complete" : "skipped") : state.stage === 2 ? "current" : "upcoming" },
+        { id: "Step 4", name: "Verifying information", status: state.stage > 3 ? "complete" : state.stage === 3 ? "current" : "upcoming" },
+        { id: "Step 5", name: "Done", status: state.stage === 4 ? "current" : "upcoming" },
     ]
 
     return (
-        <div className="h-screen bg-center bg-no-repeat bg-cover" style={{backgroundImage: response && response.data.splash && `url("${response.data.splash}")`}}>
+        <div className="h-screen bg-center bg-no-repeat bg-cover" style={{backgroundImage: state?.response?.data?.splash && `url("${state.response.data.splash}")`}}>
             <div className="py-20 md:py-40 --container">
-                <ol role="list" className={clsx("p-8 mb-20 space-y-4 bg-gray-700 rounded-lg md:flex md:space-y-0 md:space-x-8", response && response.data.splash && "shadow-2xl")}>
+                <ol role="list" className={clsx("p-8 mb-20 space-y-4 bg-gray-700 rounded-lg md:flex md:space-y-0 md:space-x-8", state?.response?.data?.splash && "shadow-2xl")}>
                     {steps.map(step => (
                         <li key={step.name} className="md:flex-1">
                             {step.status === "complete" ? (
@@ -75,20 +71,20 @@ export default function ChallengeWrapper() {
                         </li>
                     ))}
                 </ol>
-                <div className={clsx("p-8 mx-auto bg-gray-700 rounded-lg w-fit", response && response.data.splash && "shadow-2xl")}>
-                    {step === 0 && <>
+                <div className={clsx("p-8 mx-auto bg-gray-700 rounded-lg w-fit", state.response?.data?.splash && "shadow-2xl")}>
+                    {state.stage === 0 && <>
                         <h1 className="text-4xl font-bold text-center">
-                            Loading challenge data
+                            {state.error ? "Aw snap" : "Loading challenge data"}
                         </h1>
-                        <p className="mt-6 text-center text-gray-200">
+                        {state.error ? <ErrorHandler error={state.error} /> : <p className="mt-6 text-center text-gray-200">
                             This step is fully automatic, you just have to wait a bit...
-                        </p>
+                        </p>}
                     </>}
-                    {step === 1 && <>
+                    {state.stage === 1 && <>
                         <h1 className="text-4xl font-bold text-center">
                             Discord authentication
                         </h1>
-                        {response.data.logged_in && <p className="mt-6 text-center text-red-200">
+                        {state.response.data.logged_in && <p className="mt-6 text-center text-red-400">
                             You are currently logged into the wrong account.
                         </p>}
                         <p className="mt-6 text-center text-gray-200">
@@ -98,7 +94,7 @@ export default function ChallengeWrapper() {
                             Authorize
                         </InternalLink>
                     </>}
-                    {step === 2 && <>
+                    {state.stage === 2 && <>
                         <h1 className="text-4xl font-bold text-center">
                             CAPTCHA challenge
                         </h1>
@@ -112,19 +108,18 @@ export default function ChallengeWrapper() {
                                 reCaptchaCompat={false}
                                 onVerify={async token => {
                                     const { flow } = router.query;
-                                    setStep(3);
+                                    setState({ ...state, stage: 3 });
                                     try {
                                         await postChallenge(flow, token);
                                     } catch (e) {
-                                        setStep(2);
-                                        return alert(`${e.response.status}: ${e.response.data.detail}`);
+                                        return setState({ ...state, error: e });
                                     }
-                                    setStep(4);
+                                    setState({ ...state, stage: 4 });
                                 }}
                             />
                         </div>
                     </>}
-                    {step === 3 && <>
+                    {state.stage === 3 && <>
                         <h1 className="text-4xl font-bold text-center">
                             Verifying information
                         </h1>
@@ -132,7 +127,7 @@ export default function ChallengeWrapper() {
                             This step is fully automatic, you just have to wait a bit...
                         </p>
                     </>}
-                    {step === 4 && <>
+                    {state.stage === 4 && <>
                         <h1 className="text-4xl font-bold text-center">
                             Done
                         </h1>
