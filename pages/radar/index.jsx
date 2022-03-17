664b67824e355d41dd773917910c467c33e229d9
Leo Developer <git@leodev.xyz>
2022-03-17 16:57:05 +0100

    add guild-/usercount stats

--- pages/radar/index.jsx
+++ pages/radar/index.jsx
@@ -1,5 +1,5 @@
 
-import { ArrowSmDownIcon, ArrowSmRightIcon, ArrowSmUpIcon } from "@heroicons/react/solid";
+import { ArrowSmDownIcon, ArrowSmRightIcon, ArrowSmUpIcon, HomeIcon, UsersIcon } from "@heroicons/react/solid";
 import clsx from "clsx";
 
 import { useRadar } from "@/lib/api.js";
@@ -30,6 +30,15 @@ export default function Radar() {
             {isError ? <>
                 <ErrorHandler error={isError} />
             </> : <>
+                <h2 className="mt-20 mb-4 text-2xl font-bold">
+                    General statistics
+                </h2>
+                <Stats
+                    stats={[
+                        { name: "Guilds", icon: HomeIcon, stat: response && response.data.stats.guild_count },
+                        { name: "Users", icon: UsersIcon, stat: response && response.data.stats.user_count },
+                    ]}
+                />
                 <h2 className="mt-20 mb-4 text-2xl font-bold">
                     Basic trends
                 </h2>
@@ -95,14 +104,19 @@ function Stats({ stats }) {
         <dl className="grid grid-cols-1 gap-px mt-5 overflow-hidden bg-gray-800 rounded-lg shadow md:grid-cols-2 lg:grid-cols-4">
             {stats.map(item => !item.stat? <Skeleton key={item.name} className="h-28 md:h-32 lg:h-[6.75rem]" /> : (
                 <div key={item.name} className="px-4 py-5 sm:p-6 bg-gray-750">
-                    <dt className="text-base font-normal text-gray-100">{item.name}</dt>
+                    <dt className="flex text-base font-normal text-gray-100">
+                        {item.icon && <item.icon className="w-5 h-5 mr-2" />}
+                        {item.name}
+                    </dt>
                     <dd className="flex items-baseline justify-between mt-1 md:block lg:flex">
                         <div className="flex items-baseline text-2xl font-semibold text-indigo-400">
-                            {item.stat.now}
-                            <span className="ml-2 text-sm font-medium text-gray-400">from {item.stat.previous}</span>
+                            {typeof(item.stat) === "number" ? item.stat : <>
+                                {item.stat.now}
+                                <span className="ml-2 text-sm font-medium text-gray-400">from {item.stat.previous}</span>
+                            </>}
                         </div>
 
-                        <div
+                        {typeof(item.stat) !== "number" && <div
                             className={clsx(
                                 item.stat.now === item.stat.previous ? "bg-gray-100 text-gray-800" : item.stat.now <= item.stat.previous ? "bg-green-100 text-green-800" : "bg-red-100 text-red-800",
                                 "inline-flex items-baseline px-2.5 py-0.5 rounded-full text-sm font-medium md:mt-2 lg:mt-0"
@@ -118,7 +132,7 @@ function Stats({ stats }) {
 
                             <span className="sr-only">{item.stat.now > item.stat.previous ? "Increased" : "Decreased"} by</span>
                             {Math.abs(item.stat.now - item.stat.previous)}
-                        </div>
+                        </div>}
                     </dd>
                 </div>
             ))}
