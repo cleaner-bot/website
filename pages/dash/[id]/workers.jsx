937f44007ecc674631b5ae2590f87b70383700c7
Leo Developer <leodeveloper@protonmail.com>
2022-03-27 19:08:06 +0200

    remove unused imports

--- pages/dash/[id]/workers.jsx
+++ pages/dash/[id]/workers.jsx
@@ -1,12 +1,8 @@
 
-import { useRouter } from "next/router";
-
 import { useData } from "@/components/dash/data.jsx";
 import { Page, Header, Section } from "@/components/dash/dash.jsx";
-import { PlainBlock, ToggleBlock } from "@/components/dash/block.jsx";
-import { Toggle, OptionalUpgrade, BlockRightSide } from "@/components/dash/ui.jsx";
+import { ToggleBlock } from "@/components/dash/block.jsx";
 import MetaTags from "@/components/metatags.jsx";
-import { setConfig } from "next/config";
 
 export default function DashboardWrapper() {
     const data = useData();
