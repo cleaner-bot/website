f80a7d2f187ccc14a404d5a0ee2f5dbde9466be9
Leo Developer <leodeveloper@protonmail.com>
2022-04-03 13:32:52 +0200

    add a bit more margin

--- pages/index.jsx
+++ pages/index.jsx
@@ -120,7 +120,7 @@ export default function Home() {
                     <path fillOpacity="1" d="M0,128L48,133.3C96,139,192,149,288,176C384,203,480,245,576,261.3C672,277,768,267,864,234.7C960,203,1056,149,1152,133.3C1248,117,1344,139,1392,149.3L1440,160L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path>
                 </svg>
             </div>
-            <div className="--container" data-aos="fade-up">
+            <div className="my-40 --container" data-aos="fade-up">
                 <h2 className="text-6xl font-bold leading-[3rem] text-center max-w-[30rem] lg:max-w-none mx-auto">
                     Start protecting <span className="text-blue-300">your server</span> now
                 </h2>
