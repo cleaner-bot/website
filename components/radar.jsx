5a35870156b24b84bc8a087d595951819eb9a0f0
Leo Developer <leodeveloper@protonmail.com>
2022-04-14 15:15:23 +0200

    analytics rest

---
+++ components/radar.jsx
@@ -0,0 +1,53 @@
+
+import { ArrowSmDownIcon, ArrowSmRightIcon, ArrowSmUpIcon } from "@heroicons/react/solid";
+import clsx from "clsx";
+
+import Skeleton from "@/components/skeleton.jsx";
+
+
+export function Stats({ stats }) {
+    return (
+        <dl className="grid grid-cols-1 gap-px mt-5 overflow-hidden bg-gray-800 rounded-lg shadow md:grid-cols-2 lg:grid-cols-4">
+            {stats.map(item => !item ? <div /> : !item.stat ? <Skeleton key={item.name} className="h-28 md:h-32 lg:h-[6.75rem]" /> : (
+                <div key={item.name} className="px-4 py-5 sm:p-6 bg-gray-750">
+                    <dt className="flex text-base font-normal text-gray-100">
+                        {item.icon && <item.icon className="w-5 h-5 mr-2" />}
+                        {item.name}
+                    </dt>
+                    <dd className="flex items-baseline justify-between mt-1 md:block lg:flex">
+                        <div className="flex items-baseline text-2xl font-semibold text-indigo-400">
+                            {typeof(item.stat) === "number" ? formatNumber(item.stat) : <>
+                                {formatNumber(item.stat.now)}
+                                <span className="ml-2 text-sm font-medium text-gray-400">from {formatNumber(item.stat.previous)}</span>
+                            </>}
+                        </div>
+
+                        {typeof(item.stat) !== "number" && <div
+                            className={clsx(
+                                item.stat.now === item.stat.previous ? "bg-gray-100 text-gray-800" : item.stat.now <= item.stat.previous ? "bg-green-100 text-green-800" : "bg-red-100 text-red-800",
+                                "inline-flex items-baseline px-2.5 py-0.5 rounded-full text-sm font-medium md:mt-2 lg:mt-0"
+                            )}
+                        >
+                            {item.stat.now === item.stat.previous ? (
+                                <ArrowSmRightIcon className="-ml-1 mr-0.5 flex-shrink-0 self-center h-5 w-5 text-gray-500" />
+                            ) : item.stat.now > item.stat.previous ? (
+                                <ArrowSmUpIcon className="-ml-1 mr-0.5 flex-shrink-0 self-center h-5 w-5 text-red-500" />
+                            ) : (
+                                <ArrowSmDownIcon className="-ml-1 mr-0.5 flex-shrink-0 self-center h-5 w-5 text-green-500" />
+                            )}
+
+                            <span className="sr-only">{item.stat.now > item.stat.previous ? "Increased" : "Decreased"} by</span>
+                            {Math.abs(item.stat.now - item.stat.previous)}
+                        </div>}
+                    </dd>
+                </div>
+            ))}
+        </dl>
+    )
+}
+
+function formatNumber(number) {
+    let [whole, fraction] = number.toString().split(".");
+    whole = whole.split("").reverse().map((char, i) => i % 3 === 2 ? `,${char}` : char).reverse().join("").replace(/^,/g, "");
+    return fraction && fraction.length > 0 ? `${whole}.${fraction}` : whole;
+}
--- pages/dash/[id]/analytics.jsx
+++ pages/dash/[id]/analytics.jsx
@@ -2,6 +2,9 @@
 import { useData } from "@/components/dash/data.jsx";
 import { Page, Header } from "@/components/dash/dash.jsx";
 import MetaTags from "@/components/metatags.jsx";
+import ErrorHandler from "@/components/dash/error.jsx";
+import { useAnalytics } from "@/lib/api.js";
+import { range } from "@/lib/helper.js";
 
 export default function DashboardWrapper() {
     const data = useData();
@@ -19,11 +22,52 @@ export default function DashboardWrapper() {
 
 
 function AnalyticsDashboard({ config, setConfig, entitlements, guildId }) {
+    const { data: response, error: isError } = useAnalytics(guildId);
     return (
         <>
             <Header name="Analytics">
                 Look at your analytics.
             </Header>
+            {isError ? (
+                <ErrorHandler error={isError} /> 
+            ) : <>
+                <h2 className="mt-20 mb-4 text-2xl font-bold">
+                    Deployed challenges
+                </h2>
+                <p className="mb-4 text-sm text-gray-200">
+                    Trends of deployed challenges as a response to stop bad actors. Last 30 days.
+                </p>
+                <Stats
+                    stats={[
+                        { name: "Ban", stat: response && response.data.challenges.ban },
+                        { name: "Kick", stat: response && response.data.challenges.kick },
+                        { name: "Interactive challenge", stat: response && response.data.challenges.role },
+                        { name: "Timeout", stat: response && response.data.challenges.timeout },
+                    ]}
+                />
+
+                <h2 className="mt-20 mb-4 text-2xl font-bold">
+                    Triggered rules
+                </h2>
+                <p className="mb-4 text-sm text-gray-200">
+                    All rules. Last 30 days.
+                </p>
+
+                {response ? <Stats
+                    stats={Object.keys(response.data.rules).sort((a, b) => response.data.rules[b].now - response.data.rules[a].now).map(key => ({ name: key, stat: response.data.rules[key] }))}
+                /> : <Stats stats={range(12, index => ({ name: index }))} />}
+
+                <h2 className="mt-20 mb-4 text-2xl font-bold">
+                    Triggered antispam
+                </h2>
+                <p className="mb-4 text-sm text-gray-200">
+                    All traffic rules. Last 30 days.
+                </p>
+
+                {response ? <Stats
+                    stats={Object.keys(response.data.traffic).sort((a, b) => response.data.traffic[b].now - response.data.traffic[a].now).map(key => ({ name: key, stat: response.data.traffic[key] }))}
+                /> : <Stats stats={range(6, index => ({ name: index }))} />}
+            </>}
         </>
     )
 }
