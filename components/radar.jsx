3709d658de73d408f6f29af3a3c52914078e58f5
Leo Developer <leodeveloper@protonmail.com>
2022-04-14 15:48:13 +0200

    add total

--- components/radar.jsx
+++ components/radar.jsx
@@ -14,30 +14,35 @@ export function Stats({ stats }) {
                         {item.icon && <item.icon className="w-5 h-5 mr-2" />}
                         {item.name}
                     </dt>
-                    <dd className="flex items-baseline justify-between mt-1 md:block lg:flex">
-                        <div className="flex items-baseline text-2xl font-semibold text-indigo-400">
-                            {typeof(item.stat) === "number" ? formatNumber(item.stat) : <>
-                                {formatNumber(item.stat.now)}
-                                <span className="ml-2 text-sm font-medium text-gray-400">from {formatNumber(item.stat.previous)}</span>
-                            </>}
+                    <dd>
+                        <div className="flex items-baseline justify-between mt-1 md:block lg:flex">
+                            <div className="flex items-baseline text-2xl font-semibold text-indigo-400">
+                                {typeof(item.stat) === "number" ? formatNumber(item.stat) : <>
+                                    {formatNumber(item.stat.now)}
+                                    <span className="ml-2 text-sm font-medium text-gray-400">from {formatNumber(item.stat.previous)}</span>
+                                </>}
+                            </div>
+
+                            {typeof(item.stat) !== "number" && <div
+                                className={clsx(
+                                    item.stat.now === item.stat.previous ? "bg-gray-100 text-gray-800" : item.stat.now <= item.stat.previous ? "bg-green-100 text-green-800" : "bg-red-100 text-red-800",
+                                    "inline-flex items-baseline px-2.5 py-0.5 rounded-full text-sm font-medium md:mt-2 lg:mt-0"
+                                )}
+                            >
+                                {item.stat.now === item.stat.previous ? (
+                                    <ArrowSmRightIcon className="-ml-1 mr-0.5 flex-shrink-0 self-center h-5 w-5 text-gray-500" />
+                                ) : item.stat.now > item.stat.previous ? (
+                                    <ArrowSmUpIcon className="-ml-1 mr-0.5 flex-shrink-0 self-center h-5 w-5 text-red-500" />
+                                ) : (
+                                    <ArrowSmDownIcon className="-ml-1 mr-0.5 flex-shrink-0 self-center h-5 w-5 text-green-500" />
+                                )}
+
+                                <span className="sr-only">{item.stat.now > item.stat.previous ? "Increased" : "Decreased"} by</span>
+                                {Math.abs(item.stat.now - item.stat.previous)}
+                            </div>}
                         </div>
-
-                        {typeof(item.stat) !== "number" && <div
-                            className={clsx(
-                                item.stat.now === item.stat.previous ? "bg-gray-100 text-gray-800" : item.stat.now <= item.stat.previous ? "bg-green-100 text-green-800" : "bg-red-100 text-red-800",
-                                "inline-flex items-baseline px-2.5 py-0.5 rounded-full text-sm font-medium md:mt-2 lg:mt-0"
-                            )}
-                        >
-                            {item.stat.now === item.stat.previous ? (
-                                <ArrowSmRightIcon className="-ml-1 mr-0.5 flex-shrink-0 self-center h-5 w-5 text-gray-500" />
-                            ) : item.stat.now > item.stat.previous ? (
-                                <ArrowSmUpIcon className="-ml-1 mr-0.5 flex-shrink-0 self-center h-5 w-5 text-red-500" />
-                            ) : (
-                                <ArrowSmDownIcon className="-ml-1 mr-0.5 flex-shrink-0 self-center h-5 w-5 text-green-500" />
-                            )}
-
-                            <span className="sr-only">{item.stat.now > item.stat.previous ? "Increased" : "Decreased"} by</span>
-                            {Math.abs(item.stat.now - item.stat.previous)}
+                        {typeof(item.stat) !== "number" && <div className="flex items-baseline mt-2 text-2xl font-semibold text-indigo-400">
+                            Total {formatNumber(item.stat.total)}
                         </div>}
                     </dd>
                 </div>
