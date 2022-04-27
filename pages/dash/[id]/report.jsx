425af92ba3b495266b5394cfe04eb87615921b8b
Leo Developer <leodeveloper@protonmail.com>
2022-04-27 17:54:06 +0200

    add report channel select

--- pages/dash/[id]/report.jsx
+++ pages/dash/[id]/report.jsx
@@ -1,8 +1,10 @@
 
+import MetaTags from "@/components/metatags.jsx";
 import { useData } from "@/components/dash/data.jsx";
 import { Page, Header, Section } from "@/components/dash/dash.jsx";
-import { ToggleBlock } from "@/components/dash/block.jsx";
-import MetaTags from "@/components/metatags.jsx";
+import { PlainBlock, ToggleBlock } from "@/components/dash/block.jsx";
+import { DropdownSearch, Attention } from "@/components/dash/ui.jsx";
+import { doChange, patchConfig } from "@/lib/api.js";
 
 export default function DashboardWrapper() {
     const data = useData();
@@ -20,6 +22,7 @@ export default function DashboardWrapper() {
 
 
 function ReportDashboard({ config, setConfig, entitlements, guildId }) {
+    const reportChannel = guild.channels.find(channel => channel.id === config.report_channel);
     return (
         <>
             <Header name="Report">
@@ -36,6 +39,35 @@ function ReportDashboard({ config, setConfig, entitlements, guildId }) {
                     entitlement={entitlements.report}
                     entitlements={entitlements}
                 />
+                {config.report_enabled && <>
+                    <PlainBlock
+                        name="Report channel"
+                        description="Select the channel where reports will be sent to."
+                    >
+                        <DropdownSearch
+                            placeholder="Select channel."
+                            values={guild.channels.filter(channel => guild.myself.permissions.ADMINISTRATOR || (channel.permissions.VIEW_CHANNEL && channel.permissions.SEND_MESSAGES && channel.permissions.EMBED_LINKS))}
+                            current={config.report_channel}
+                            setCurrent={async new_value => {
+                                const success = await doChange(patchConfig(guildId, {report_channel: new_value}));
+                                if(!success) return;
+                                setConfig({...config, report_channel: new_value});
+                            }}
+                        />
+                        <p className="my-6 text-sm text-gray-300">
+                            Channel not listed? Make sure The Cleaner can send messages and embeds in it.
+                        </p>
+                        {config.report_channel === "0" && <Attention>
+                            No report channel selected.
+                        </Attention>}
+                        {!reportChannel && config.report_channel !== "0" && <Attention>
+                            The report channel has been deleted. Please select a new one.
+                        </Attention>}
+                        {reportChannel && !(reportChannel.permissions.ADMINISTRATOR || (reportChannel.permissions.VIEW_CHANNEL && reportChannel.permissions.SEND_MESSAGES && reportChannel.permissions.EMBED_LINKS)) && <Attention>
+                            The Cleaner can not send messages and embeds in the current report channel. Please select a new one or give me the missing permissions.
+                        </Attention>}
+                    </PlainBlock>
+                </>}
             </Section>
         </>
     )
