5ad3d8eaef3f8d98a05b1cc8d34df31b4a515e61
Leo Developer <leodeveloper@protonmail.com>
2022-04-29 22:06:28 +0200

    add /billing/stripe

---
+++ pages/billing/stripe/index.jsx
@@ -0,0 +1,30 @@
+import { useRouter } from "next/router";
+
+import MetaTags from "@/components/metatags.jsx";
+import { doChange, getStripePortalURL } from "@/lib/api.js";
+
+export default function Cancelled() {
+    const router = useRouter();
+    const { guild } = router.query;
+    return (
+        <div className="flex items-center justify-center h-screen mx-4">
+            <MetaTags title="Payment successful | Stripe" />
+            <div className="px-8 py-4 border border-green-500 rounded-lg bg-green-400/10">
+                <button
+                    className="--btn --btn-3 --btn-neutral"
+                    onClick={async () => {
+                        const response = await doChange(getStripePortalURL(), {
+                            loading: "Preparing portal...",
+                            success: "Redirecting...",
+                            error: "Portal failed: "
+                        });
+                        if(!response) return;
+                        router.push(response.data);
+                    }}
+                >
+                    Access billing portal
+                </button>
+            </div>
+        </div>
+    )
+}
