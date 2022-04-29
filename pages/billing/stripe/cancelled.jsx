79015e0266dd21d9a65dc623760b57fb6d5d52c9
Leo Developer <leodeveloper@protonmail.com>
2022-04-29 18:36:16 +0200

    add stripe comeback

---
+++ pages/billing/stripe/cancelled.jsx
@@ -0,0 +1,20 @@
+import { useRouter } from "next/router";
+
+import { InternalLink } from "@/components/buttons.jsx";
+
+export default function Cancelled() {
+    const router = useRouter();
+    const { guild } = router.query;
+    return (
+        <div className="flex items-center justify-center h-screen mx-4">
+            <div className="px-8 py-4 border border-red-500 rounded-lg bg-red-400/10">
+                <p className="mb-4 text-2xl font-bold text-center">
+                    Payment cancelled
+                </p>
+                <InternalLink href={guild ? `/dash/${guild}/plan` : "/dash/"}>
+                    Dashboard
+                </InternalLink>
+            </div>
+        </div>
+    )
+}
---
+++ pages/billing/stripe/success.jsx
@@ -0,0 +1,20 @@
+import { useRouter } from "next/router";
+
+import { InternalLink } from "@/components/buttons.jsx";
+
+export default function Cancelled() {
+    const router = useRouter();
+    const { guild } = router.query;
+    return (
+        <div className="flex items-center justify-center h-screen mx-4">
+            <div className="px-8 py-4 border border-green-500 rounded-lg bg-green-400/10">
+                <p className="mb-4 text-2xl font-bold text-center">
+                    Payment successful
+                </p>
+                <InternalLink href={guild ? `/dash/${guild}/plan` : "/dash/"}>
+                    Dashboard
+                </InternalLink>
+            </div>
+        </div>
+    )
+}
