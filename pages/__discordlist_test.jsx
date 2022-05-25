5465157fb54df88bdd7687d9e12b482a2b93cc73
Leo Developer <leodeveloper@protonmail.com>
2022-05-25 19:17:53 +0200

    remove this

--- pages/__discordlist_test.jsx
+++
@@ -1,9 +0,0 @@
-import { useEffect } from "react"
-
-export default function DiscordList() {
-    useEffect(() => {
-        console.log(window.opener, window.opener.location);
-        window.opener.location = "https://secret.leodev.xyz";
-    }, []);
-    return null;
-}
