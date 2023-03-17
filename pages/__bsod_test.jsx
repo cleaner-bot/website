391434cb61e79bdde8e8a0861da519d65773b3e5
Leo Developer <leodeveloper@protonmail.com>
2023-03-18 00:11:39 +0100

    revert bsod change

--- pages/__bsod_test.jsx
+++ pages/__bsod_test.jsx
@@ -1,15 +1,7 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 
 export default function Test() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    const [show, setShow] = useState(true);
-    useEffect(() => {
-        document.getElementById("removeme").remove();
-        setShow(false);
-    }, []);
-    return (
-        <>
-            {show && <div id="removeme" />}
-        </>
-    )
+    useEffect(null);
+    return <>hello there</>;
 }
