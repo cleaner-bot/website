f95c9c1df4840b563b5948c0e254688228c6f33f
Leo Developer <leodeveloper@protonmail.com>
2023-03-18 01:43:55 +0100

    fix header

--- components/header.jsx
+++ components/header.jsx
@@ -9,10 +9,10 @@ const headerLinks = [
 
 export default function Header() {
     return (
-        <div className="flex items-center justify-center gap-4 py-4 border-b border-gray-550">
+        <div className="flex items-center justify-center gap-4 py-4 border-b lg:gap-8 border-gray-550">
             <Link
                 href="/"
-                className="flex items-center justify-center h-full text-2xl font-extrabold hover:underline decoration-indigo-550 decoration-4 underline-offset-2 sm:mr-12"
+                className="flex items-center justify-center h-full text-2xl font-extrabold hover:underline decoration-indigo-550 decoration-4 underline-offset-2"
             >
                 <Image
                     className="inline-block w-8 h-8 mr-2 rounded-full sm:hidden md:inline-block"
@@ -23,12 +23,12 @@ export default function Header() {
                 />
                 The Cleaner
             </Link>
-            <div className="hidden gap-4 sm:flex">
+            <div className="hidden sm:flex lg:gap-4">
                 {headerLinks.map((x) => (
                     <Link
                         href={x.href}
                         key={x.href}
-                        className="--btn hover:bg-gray-550 --btn-3"
+                        className="--btn hover:bg-gray-550 --btn-1"
                     >
                         {x.name}
                     </Link>
